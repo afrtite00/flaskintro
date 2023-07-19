b122bdfd4e70ea3230f2e485abd8e20e3212c26e
Abdellghafore afrtite <112224034+afrtite00@users.noreply.github.com>
2023-07-19 17:33:22 +0100

    edit

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 def main():
-  return "hello abdo and welcome to your new app , congrats on your first ever push"
-
+  return render_template("index.html")
 
 if __name__ == "__main__" :
   app.run(host="0.0.0.0",debug= True)
